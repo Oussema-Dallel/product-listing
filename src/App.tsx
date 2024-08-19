71b241783dd735f904447ab70935883e5d3cad66
Oussema Dallel <oussemadallel@gmail.com>
2024-08-19 03:08:20 +0200

    feat: add more documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { FilterProducts } from './components/FilterProducts/FilterProducts';
 import { Layout } from './components/Layout/Layout';
 import { ProductList } from './components/ProductListing/ProductList';
+import { Typography } from '@mui/material';
 import type { FunctionComponent, ReactElement } from 'react';
 
 const App: FunctionComponent = (): ReactElement => {
 	return (
 		<Layout>
-			<h1>The most amazing product list application</h1>
+			<Typography
+				gutterBottom
+				marginBottom='2rem'
+				variant='h4'
+			>
+				The most amazing product list application
+			</Typography>
 			<FilterProducts />
 			<ProductList />
 		</Layout>
--- src/mocks/enableMocking.ts
+++ src/mocks/enableMocking.ts
@@ -1,3 +1,8 @@
+/**
+ * A utility function to register and enable a service worker to intercept all requests registered
+ * through the handlers. It only works in the development environment.
+ * @returns the registered and activated worker
+ */
 const enableMocking = async (): Promise<ServiceWorkerRegistration | undefined> => {
 	if (!import.meta.env.DEV) {
 		return;
